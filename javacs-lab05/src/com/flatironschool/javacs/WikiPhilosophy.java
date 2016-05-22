e49e4371c0e8c4ce334000a900645313123f869c
apatronl <apl7@gatech.edu>
2016-05-22 15:44:15 -0700

    Done.

--- javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
+++ javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
@@ -11,41 +11,98 @@ import org.jsoup.nodes.TextNode;
 import org.jsoup.select.Elements;
 
 public class WikiPhilosophy {
-	
+
 	final static WikiFetcher wf = new WikiFetcher();
-	
+
 	/**
 	 * Tests a conjecture about Wikipedia and Philosophy.
-	 * 
+	 *
 	 * https://en.wikipedia.org/wiki/Wikipedia:Getting_to_Philosophy
-	 * 
+	 *
 	 * 1. Clicking on the first non-parenthesized, non-italicized link
      * 2. Ignoring external links, links to the current page, or red links
      * 3. Stopping when reaching "Philosophy", a page with no links or a page
      *    that does not exist, or when a loop occurs
-	 * 
+	 *
 	 * @param args
 	 * @throws IOException
 	 */
 	public static void main(String[] args) throws IOException {
-		
-        // some example code to get you started
 
 		String url = "https://en.wikipedia.org/wiki/Java_(programming_language)";
-		Elements paragraphs = wf.fetchWikipedia(url);
+		String baseURL = "https://en.wikipedia.org";
+		List<String> visited = new ArrayList<String>();
+		boolean foundPhilosophy = false;
 
-		Element firstPara = paragraphs.get(0);
-		
-		Iterable<Node> iter = new WikiNodeIterable(firstPara);
-		for (Node node: iter) {
-			if (node instanceof TextNode) {
-				System.out.print(node);
+		while (!foundPhilosophy) {
+			visited.add(url);
+			Elements paragraphs = wf.fetchWikipedia(url);
+			Element firstPara = paragraphs.get(0);
+			Iterable<Node> iter = new WikiNodeIterable(firstPara);
+
+			String validURL = getURL(iter, url);
+			String completeURL = baseURL + validURL;
+			if (validURL != null) {
+				// We got to Philosophy!
+				if (completeURL.equals("https://en.wikipedia.org/wiki/Philosophy")) {
+					visited.add(completeURL);
+					foundPhilosophy = true;
+				} else {
+					url = completeURL;
+				}
+			} else {
+				// Not really found philosophy, just ended up in a loop
+				// with no valid URLs
+				foundPhilosophy = true;
+				System.out.println("ERROR");
 			}
+		}
+
+		// Output visited URLs
+		for (String visitedURL: visited) System.out.println(visitedURL);
+	}
+
+	// Get a "valid" URL, returns null if not found
+	private static String getURL(Iterable<Node> iter, String currentURL) {
+		for (Node node : iter) {
+			if (node instanceof Element) {
+				Element curr = (Element)node;
+				if (isValid(curr, currentURL)) {
+					return node.attr("href");
+				}
+			}
+		}
+		return null;
+	}
+
+	private static boolean isValid(Element e, String currentURL) {
+		String url = "https://en.wikipedia.org" + e.attr("href");
+
+		Element temp = e;
+
+		// Check italics
+		while (temp != null) {
+			if (temp.tagName().equals("i") || temp.tagName().equals("em")) {
+				return false;
+			}
+			temp = temp.parent();
+		}
+
+		// Some attributes return empty strings (not valid)
+		if (e.attr("href").equals("")) {
+			return false;
+		}
+
+		// Skip citations or pronunciation help URLs
+		if (e.attr("href").startsWith("/wiki/Help:") || e.attr("href").startsWith("#cite_note")){
+            return false;
         }
 
-        // the following throws an exception so the test fails
-        // until you update the code
-        String msg = "Complete this lab by adding your code and removing this statement.";
-        throw new UnsupportedOperationException(msg);
+		// Link to the current page
+		if (url.equals(currentURL)) {
+			return false;
+		}
+
+		return true;
 	}
 }
